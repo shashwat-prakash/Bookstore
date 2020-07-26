5c5c7b7edbb4c7c716f9b41f4b37aa345949047c
Shashwat Prakash <kumar.prince502@gmail.com>
2020-07-26 14:21:18 +0530

    Initial Commit

---
+++ Bookstore/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Bookstore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            /*app.Use(async (context, next)=>
+            {
+                await context.Response.WriteAsync("Welcome to 1st Middleware.");
+                await next();
+                await context.Response.WriteAsync("Response from 1st Middleware.");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Welcome from 2nd Middleware.");
+                await next();
+            });*/
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            /*app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/Shashwat", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Shashwat!");
+                });
+            });*/
+        }
+    }
+}
