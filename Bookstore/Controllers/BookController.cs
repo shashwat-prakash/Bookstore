e6e0fdc159fd64ecf9aeef2834ad202da1c570a4
Shashwat Prakash <kumar.prince502@gmail.com>
2020-07-26 20:40:39 +0530

    Samle BookController Added and Tested.

---
+++ Bookstore/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book id is: { id }";
+        }
+
+        //http://localhost:5000/book/searchbook?bookname=MVC&authorname=Shashwat_Prakash
+        public string SearchBook(string BookName, string AuthorName)
+        {
+            return $"Bookname: {BookName}, Author name:{AuthorName}";
+        }
+    }
+}
