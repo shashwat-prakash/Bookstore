742d0d4eb51be0a35e6f24402ce5930b60a40c13
Shashwat Prakash <kumar.prince502@gmail.com>
2020-07-31 21:02:36 +0530

    About Validation Summary helper tag, asp-validation-summary which value can be All, ModelOnly, and None

--- Bookstore/Controllers/BookController.cs
+++ Bookstore/Controllers/BookController.cs
@@ -36,6 +36,7 @@ namespace Bookstore.Controllers
             }
             /*ViewBag.isSuccess = isSuccess;
             ViewBag.BookId = bookId;*/
+            ModelState.AddModelError("", "Error from Book Controller");
             return View();
         }
         public async Task<ViewResult> GetAllBooks()
